b493f2bff3d7a95c15dada3590619fce3df23b55
Rogerio CS <Rogerio CS>
2017-07-19 22:49:17 -0300

    Criando o Primeiro Serviço

---
+++ cursoFrameWeb/backend/ComandTerminal.js
@@ -0,0 +1,50 @@
+/*
+
+
+--COMANDOS MONGODB--
+
+Iniciar comandos mongo
+mongo
+
+Usar um banco
+use + nome do banco
+Ex:
+use mecanica
+
+Exibir collections
+show collectios
+
+Exibir dados das collections
+db.clientes.find().pretty()
+
+--COMANDOS TERMINAL NODE CRIADO DE PROJETO--
+
+Abrir Diretorio
+cd desktop
+
+Criar e acessar a pasta 
+mkdir cursoFrameWeb && cd cursoFrameWeb
+mkdir backend && cd backend
+
+Iniciando o projeto com node
+npm init
+
+Preencher as informações do package.json
+
+Visualizar as informações que foram criadas dentro da pasta
+dir
+
+Instalando dependencias do projeto
+npm install express body-parser mongoose node-restful mongoose-paginate lodash express-query-int pm2 --save
+
+Instalando o nedemon para desenvolvimento
+npm install nodemon --save-dev
+
+Iniciando IDE
+code .
+
+Rodando a aplicação no Node 
+npm run dev
+
+
+*/
