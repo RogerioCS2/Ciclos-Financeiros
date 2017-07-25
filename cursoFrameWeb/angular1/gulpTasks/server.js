0c22157449d025499b7267010910279a0d15e9d1
Rogerio CS <Rogerio CS>
2017-07-24 21:03:42 -0300

    Html Inicial do Projeto (Template)

--- cursoFrameWeb/angular1/gulpTasks/server.js
+++ cursoFrameWeb/angular1/gulpTasks/server.js
@@ -10,7 +10,7 @@ gulp.task('server', ['watch'], function() {
     }))
 })
 
-gulp.task('server', function() {
+gulp.task('watch', function() {
     watch('app/**/*.html', () => gulp.start('app.html'))
     watch('app/**/*.css', () => gulp.start('app.css'))
     watch('app/**/*.js', () => gulp.start('app.js'))
