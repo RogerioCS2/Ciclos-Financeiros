a3ca7d0ccf4260fc4b9330336631e7cd81481dd8
Leandro Reolon <you@example.com>
2017-08-01 14:06:16 -0300

    Correção cadastro

--- cursoFrameWeb/angular1/app/billingCycles/billingCyclesController.js
+++ cursoFrameWeb/angular1/app/billingCycles/billingCyclesController.js
@@ -14,12 +14,12 @@
             $http.get(url).then(function(response) {
 
                 vm.billingCycle = {}
-                vm.billingCycles = response
+                vm.billingCycles = response.data
             })
         }
 
         vm.create = function() {
-            $http.post(url, vm.billingCycles).then(function(response) {
+            $http.post(url, vm.billingCycle).then(function(response) {
                 vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!!')
             }).catch(function(data) {
